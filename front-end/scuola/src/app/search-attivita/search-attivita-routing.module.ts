6e7a25d80fa0b9155a15627b0b88d9735d8ef883
Nawaz Khurshid <nawaz1981@gmail.com>
2020-04-17 12:38:36 +0200

    Fix for 2-179
    refacforation

--- front-end/scuola/src/app/search-attivita/search-attivita-routing.module.ts
+++ front-end/scuola/src/app/search-attivita/search-attivita-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SearchAttivitaComponent } from './search-attivita.component';
-import { RicercaModificaCompetenzeComponent } from './ricerca-modifica-competenze/ricerca-modifica-competenze.component';
 
 const routes: Routes = [
   {
     path: '', component: SearchAttivitaComponent,
     children: [
-      { path: 'modificacompetenze', component: RicercaModificaCompetenzeComponent },
-
     ]
   }
 ];
@@ -19,7 +16,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class ProgrammaRoutingModule {
-  static components = [SearchAttivitaComponent, RicercaModificaCompetenzeComponent];
+  static components = [SearchAttivitaComponent];
 }
 
 
