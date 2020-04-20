61b50ad813e5ad8fedd6e0637f09686136e56664
micnori <micnori@yahoo.com>
2020-04-20 16:54:43 +0200

    fix cors

--- back-end/src/main/java/it/smartcommunitylab/cartella/asl/config/AppConfig.java
+++ back-end/src/main/java/it/smartcommunitylab/cartella/asl/config/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig implements WebMvcConfigurer {
 	
 	@Override
   public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**");
+      registry.addMapping("/**").allowedMethods("*");
   }
 
 	@Override
