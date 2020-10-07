fa5e33953dd1a73a6ba5035a3806a1c8967cff95
Nawaz Khurshid <nawaz1981@gmail.com>
2020-10-07 15:08:12 +0200

    Update attivita-routing.module.ts

--- front-end/azienda/src/app/attivita/attivita-routing.module.ts
+++ front-end/azienda/src/app/attivita/attivita-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AttivitaComponent } from './attivita.component';
 import { AttivitaContainerComponent } from './attivita-container.component';
-// import { AttivitaDettaglioComponent } from './actions/attivita-dettaglio/attivita-dettaglio.component';
-// import { AttivitaDettaglioModificaComponent } from './actions/attivita-modifica-dettaglio/attivita-modifica.component';
+import { AttivitaDettaglioComponent } from './actions/attivita-dettaglio/attivita-dettaglio.component';
+import { AttivitaDettaglioModificaComponent } from './actions/attivita-modifica-dettaglio/attivita-modifica.component';
 // import { DocumentoCancellaModal } from './actions/documento-cancella-modal/documento-cancella-modal.component';
-// import { GestionePresenzeIndividualeComponent } from './actions/gestione-presenze-individuale/gestione-preseneze-individuale.component';
+import { GestionePresenzeIndividualeComponent } from './actions/gestione-presenze-individuale/gestione-preseneze-individuale.component';
 // import { GestionePresenzeGruppoComponent } from './actions/gestione-presenze-gruppo/gestione-preseneze-gruppo.component';
 
 const routes: Routes = [
@@ -14,8 +14,8 @@ const routes: Routes = [
     path: '', component: AttivitaContainerComponent,
     children: [
       { path: 'list', component: AttivitaComponent },
-      // { path: 'detail/:id', component: AttivitaDettaglioComponent },
-      // { path: 'detail/:id/modifica/attivita', component: AttivitaDettaglioModificaComponent },
+      { path: 'detail/:id', component: AttivitaDettaglioComponent },
+      { path: 'detail/:id/modifica/attivita', component: AttivitaDettaglioModificaComponent },
       // { path: 'detail/:id/modifica/studenti/presenze/individuale', component: GestionePresenzeIndividualeComponent },
       // { path: 'detail/:id/modifica/studenti/presenze/gruppo', component: GestionePresenzeGruppoComponent },
       { path: '**', pathMatch:'full', redirectTo: 'list' }
@@ -31,10 +31,10 @@ export class AttivitaRoutingModule {
   static components = [
     AttivitaContainerComponent,
     AttivitaComponent,
-    // AttivitaDettaglioComponent,
+    AttivitaDettaglioComponent,
+    AttivitaDettaglioModificaComponent,
+    GestionePresenzeIndividualeComponent,
     // DocumentoCancellaModal,
-    // AttivitaDettaglioModificaComponent,
-    // GestionePresenzeIndividualeComponent,
     // GestionePresenzeGruppoComponent
   ];
 }
