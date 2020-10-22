b3229870ffee5359c90e852e09b9effd1fd2732e
Nawaz Khurshid <nawaz1981@gmail.com>
2020-10-22 09:02:59 +0200

    Update offerte-routing.module.ts

--- front-end/azienda/src/app/offerte/offerte-routing.module.ts
+++ front-end/azienda/src/app/offerte/offerte-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { OfferteComponent } from './offerte.component';
 import { OfferteContainerComponent } from './offerte-container.component';
 import { CreaOffertaModalComponent } from './actions/crea-offerta-modal/crea-offerta-modal.component';
+import { OfferteDettaglioComponent } from './actions/offerte-dettaglio/offerte-dettaglio.component';
 
 const routes: Routes = [
   {
     path: '', component: OfferteContainerComponent,
     children: [
       { path: 'list', component: OfferteComponent },
+      { path: 'detail/:id', component: OfferteDettaglioComponent },
       { path: '**', pathMatch:'full', redirectTo: 'list' }
     ]
   }
@@ -24,6 +26,7 @@ export class OfferteRoutingModule {
     OfferteContainerComponent,
     OfferteComponent,
     CreaOffertaModalComponent,
+    OfferteDettaglioComponent,
   ];
 }
 
