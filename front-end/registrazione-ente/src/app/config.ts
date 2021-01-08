7d6e95e2d07b5b4e0e43583681c278151476f8aa
Nawaz Khurshid <nawaz1981@gmail.com>
2021-01-08 17:30:25 +0100

    Fix for asl2-343

---
+++ front-end/registrazione-ente/src/app/config.ts
@@ -0,0 +1,7 @@
+export var config = {
+  defaultPosition: { 'latitude': 46.1025748, 'longitude': 10.927261 },
+  aacClientId: "7175611b-cb56-4d76-b516-ee2df4461711",
+  redirectUrl: "http://localhost:4400/",
+  scope: "profile.basicprofile.me profile.accountprofile.me",
+  aacUrl: "https://aac.platform.smartcommunitylab.it/aac/"
+}
---
+++ front-end/registrazione-ente/src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { DataService } from '../services/data.service';
+import { AuthenticationService } from '../services/authentication.service';
+import { environment } from '../../../environments/environment';
+
+@Component({
+    selector: 'cm-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit, OnDestroy {
+
+    isCollapsed: boolean;
+    loginLogoutText: string = 'Login';
+    sub: Subscription;
+    isSticky: boolean = false;
+    profile;
+    aziende;
+    actualAzienda;
+    profileDropdownVisible;
+    env = environment;
+  
+    @Output() isStickyListener = new EventEmitter<boolean>();
+
+    constructor(private router: Router, public dataService: DataService, private authService: AuthenticationService, private _eref: ElementRef) { }
+
+    ngOnInit() {
+
+        
+    }
+    
+    initProfile() {
+        this.dataService.getProfile().subscribe(profile => {
+            if (profile) {
+                this.profile = profile;
+                if (profile && profile.aziende) {
+                    this.dataService.setAziendaId('Fondazione Bruno Kessler');
+                    this.dataService.setAziendaName('Fondazione Bruno Kessler');
+                }
+            }
+        })
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    loginOrOut() {
+
+    }
+
+    logout() {
+        this.authService.logout();
+    }
+
+    redirectToLogin() {
+        this.router.navigate(['/login']);
+    }
+
+    setLoginLogoutText() {
+    }
+
+    isActive(location) {
+        if (this.router.url.includes(location)) {
+            return true;
+        }
+    }
+
+}
