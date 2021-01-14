c189766b4a63e59989beb0b3fbb7a26078be4c89
micnori <micnori@yahoo.com>
2021-01-14 16:05:55 +0100

    send also html body part

--- back-end/src/main/java/it/smartcommunitylab/cartella/asl/services/MailService.java
+++ back-end/src/main/java/it/smartcommunitylab/cartella/asl/services/MailService.java
@@ -6,12 +6,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.mail.BodyPart;
 import javax.mail.Message;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -50,8 +54,7 @@ public class MailService {
 		props.put("mail.smtp.port", mailPort);  
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.ssl.enable", "true");
-		props.put("mail.smtp.socketFactory.class",    
-		          "javax.net.ssl.SSLSocketFactory");  
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");  
 		props.put("mail.smtp.localhost", "climb.smartcommunitylab.it");
 		Session session = Session.getInstance(props,    
 		  new javax.mail.Authenticator() {    
@@ -65,30 +68,46 @@ public class MailService {
 
 	private void sendEmail(String to, String subject, String template, Map<String, String> vars) throws Exception {
 		try {
-			BufferedReader buf = new BufferedReader(
-					new InputStreamReader(Thread.currentThread().getContextClassLoader()
-							.getResourceAsStream("templates/mail/" + template + ".txt"), "UTF-8"));
-			StringBuilder sb = new StringBuilder();
-			String line;
-			while((line = buf.readLine()) != null) {
-				sb.append(line).append("\n");
-			}
-			String contentString = sb.toString();
-			for(String key : vars.keySet()) {
-				contentString = contentString.replace("{{" + key + "}}", vars.get(key));
-			}
+			String textMesage = parseTemplate("templates/mail/" + template + ".txt", vars);
+			String htmlMessage = parseTemplate("templates/mail/" + template + ".html", vars);
+			Multipart multipart = new MimeMultipart();
+			// PLAIN TEXT
+			BodyPart messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setText(textMesage);
+			multipart.addBodyPart(messageBodyPart);
+			// HTML TEXT
+			messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setContent(htmlMessage, "text/html");
+			multipart.addBodyPart(messageBodyPart);
+			
 			Session session = setMailerSession();
 			MimeMessage message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(mailFrom));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));    
       message.setSubject(subject);    
-			message.setText(contentString, "UTF-8");
+			message.setContent(multipart);
 			Transport.send(message);
 			logger.info(String.format("sendEmail:%s - %s", to, subject));
 		} catch (Exception e) {
 			logger.warn(String.format("sendEmail error:%s", e.getMessage()));
 			throw new ServiceException("sendEmail error:" + e.getMessage());
 		}
+	}
+	
+	private String parseTemplate(String template, Map<String, String> vars) throws Exception {
+		BufferedReader buf = new BufferedReader(
+				new InputStreamReader(Thread.currentThread().getContextClassLoader()
+						.getResourceAsStream(template), "UTF-8"));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while((line = buf.readLine()) != null) {
+			sb.append(line).append("\n");
+		}
+		String contentString = sb.toString();
+		for(String key : vars.keySet()) {
+			contentString = contentString.replace("{{" + key + "}}", vars.get(key));
+		}
+		return contentString; 
 	}
 	
 	public void inviaRichiestaRegistrazione(RegistrazioneEnte reg) throws Exception {
