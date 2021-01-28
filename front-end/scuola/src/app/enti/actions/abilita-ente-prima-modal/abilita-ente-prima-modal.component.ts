b37a67b2dfc2976129fc1efdb4c32b4d5a80dd55
Nawaz Khurshid <nawaz1981@gmail.com>
2021-01-28 09:35:39 +0100

    Fix for asl2-352

---
+++ front-end/scuola/src/app/enti/actions/abilita-ente-prima-modal/abilita-ente-prima-modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'abilita-ente-prima-modal',
+  templateUrl: './abilita-ente-prima-modal.html',
+  styleUrls: ['./abilita-ente-prima-modal.scss']
+})
+export class AbilitaEntePrimaModal {
+  closeResult: string;
+
+  @Input() ente;
+  @Output() onAbilita = new EventEmitter<string>();
+
+  constructor(public activeModal: NgbActiveModal) { }
+ 
+
+}
+
