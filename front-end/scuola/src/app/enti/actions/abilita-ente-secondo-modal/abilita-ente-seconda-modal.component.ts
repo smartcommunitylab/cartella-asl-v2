e4af4b66853c44a467c77f7a252ff906c72ebaa6
Nawaz Khurshid <nawaz1981@gmail.com>
2021-01-29 09:47:34 +0100

    Fix for asl2-352

---
+++ front-end/scuola/src/app/enti/actions/abilita-ente-secondo-modal/abilita-ente-seconda-modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'abilita-ente-seconda-modal',
+  templateUrl: './abilita-ente-seconda-modal.html',
+  styleUrls: ['./abilita-ente-seconda-modal.scss']
+})
+export class AbilitaEnteSecondaModal {
+  closeResult: string;
+
+  @Input() ente;
+  @Output() onAbilita = new EventEmitter<string>();
+
+  constructor(public activeModal: NgbActiveModal) { }
+  delete() {
+    this.activeModal.close();
+    this.onAbilita.emit('ABILITA');
+  }
+}
+
