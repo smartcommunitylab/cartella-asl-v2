f4913561b8721d0fb4f9341f829409fb9dbf3769
Nawaz Khurshid <nawaz1981@gmail.com>
2021-04-20 16:32:59 +0200

    Fix for asl2-379

--- front-end/scuola/src/app/istituto/istituto.component.ts
+++ front-end/scuola/src/app/istituto/istituto.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Location } from '@angular/common';
 import { environment } from '../../environments/environment';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
     selector: 'istituto',
@@ -20,6 +22,37 @@ export class IstitutoComponent implements OnInit {
     timeoutTooltip = 250;
     env = environment;
     istituto;
+	
+	 pieChartLabels: Label[] = [];
+  pieChartData: number[] = [];
+  pieChartType: ChartType = 'pie';
+  pieChartLegend = true;
+  pieChartPlugins = [];
+  pieChartColors = [
+    {
+      backgroundColor: ['#FF9700', '#00CF86', '#0066CC', '#F83E5A', '#7FB2E5', '#5A6772', '#E5E5E5', '#5A41F3', '#05FF00', '#FF6AA0', '#00FFFF', '#AD00FF', '#04111C'],
+    },
+  ];
+
+  // Classe stack bar graph.
+  public barChartClasseOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartClasseLabels: Label[];
+  public barChartClasseType: ChartType = 'bar';
+  public barChartClasseLegend = true;
+  public barChartClassePlugins = [];
+  public barChartClasseData: ChartDataSets[];
+
+  // Istituto bar graph.
+  public barChartIstitutoOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartIstitutoLabels: Label[];
+  public barChartIstitutoType: ChartType = 'bar';
+  public barChartIstitutoLegend = true;
+  public barChartIstitutoPlugins = [];
+  public barChartIstitutoData: ChartDataSets[];
 
     constructor(
         private dataService: DataService,
