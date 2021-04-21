91e085bfd0a5f42bbc562950e6e584311b68e4d8
Nawaz Khurshid <nawaz1981@gmail.com>
2021-04-21 14:10:12 +0200

    Fix for asl2-380

--- front-end/scuola/src/app/istituto/istituto.component.ts
+++ front-end/scuola/src/app/istituto/istituto.component.ts
@@ -8,22 +8,57 @@ import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
 @Component({
-    selector: 'istituto',
-    templateUrl: './istituto.component.html',
-    styleUrls: ['./istituto.component.scss']
+  selector: 'istituto',
+  templateUrl: './istituto.component.html',
+  styleUrls: ['./istituto.component.scss']
 })
 
 export class IstitutoComponent implements OnInit {
-    filtro;
-    filterSearch = false;
-    closeResult: string;
-    menuContent = "In questa sezione trovi una dashboard che mostra un riassunto visivo delle attività di tutti gli studenti. Inoltre, da qui puoi gestire gli account abilitati sul Portale EDIT Istituto di questo istituto e i dati principali dell’istituto.";
-    showContent: boolean = false;
-    timeoutTooltip = 250;
-    env = environment;
-    istituto;
-	
-	 pieChartLabels: Label[] = [];
+  filtro;
+  filterSearch = false;
+  closeResult: string;
+  menuContent = "In questa sezione trovi una dashboard che mostra un riassunto visivo delle attività di tutti gli studenti. Inoltre, da qui puoi gestire gli account abilitati sul Portale EDIT Istituto di questo istituto e i dati principali dell’istituto.";
+  showContent: boolean = false;
+  timeoutTooltip = 250;
+  env = environment;
+  istituto;
+  tipologie;
+  numeroAttivitaInAttesa;
+  numeroAttivitaInCorso;
+  numeroAttivitaInRevisione;
+  oreTotali;
+
+  pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'left',
+    },
+    tooltips: {
+      enabled: true,
+      mode: 'single',
+      callbacks: {
+        label: function (tooltipItems, data) {
+          var multistringText = [];
+          multistringText.push(data.labels[tooltipItems.index]);
+          
+          return multistringText;
+        },
+        footer: function (tooltipItems, data) {
+          var multistringText = [];
+          // calculate ore totali.
+          var oreTotali = 0;
+          Object.keys(data.datasets[0].data).map(key => {
+            oreTotali = oreTotali + data.datasets[0].data[key];
+          });
+          multistringText.push(data.datasets[0].data[tooltipItems[0].index] + ' ore');
+          multistringText.push(Math.round(Number(data.datasets[0].data[tooltipItems[0].index])/oreTotali * 100) + ' % del totali')
+
+          return multistringText;
+        }
+      }
+    },
+  };
+  pieChartLabels: Label[] = [];
   pieChartData: number[] = [];
   pieChartType: ChartType = 'pie';
   pieChartLegend = true;
@@ -34,55 +69,61 @@ export class IstitutoComponent implements OnInit {
     },
   ];
 
-  // Classe stack bar graph.
-  public barChartClasseOptions: ChartOptions = {
-    responsive: true,
-  };
-  public barChartClasseLabels: Label[];
-  public barChartClasseType: ChartType = 'bar';
-  public barChartClasseLegend = true;
-  public barChartClassePlugins = [];
-  public barChartClasseData: ChartDataSets[];
 
-  // Istituto bar graph.
-  public barChartIstitutoOptions: ChartOptions = {
-    responsive: true,
-  };
-  public barChartIstitutoLabels: Label[];
-  public barChartIstitutoType: ChartType = 'bar';
-  public barChartIstitutoLegend = true;
-  public barChartIstitutoPlugins = [];
-  public barChartIstitutoData: ChartDataSets[];
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private modalService: NgbModal
+  ) {
 
-    constructor(
-        private dataService: DataService,
-        private route: ActivatedRoute,
-        private router: Router,
-        private location: Location,
-        private modalService: NgbModal
-    ) {
+  }
 
-    }
+  ngOnInit(): void {
+    this.getIstituto();
+     // GET classes.
+     this.dataService.getAttivitaTipologie().subscribe((res) => {
+      this.tipologie = res;
+      var report;
+      this.initPieChart(report);
+     });
 
-    ngOnInit(): void {
-        this.getIstituto();
-    }
+  }
 
-    getIstituto() {
-        this.dataService.getIstitutoById(this.dataService.istitutoId)
-            .subscribe((response) => {
-                this.istituto = response;
-            },
-                (err: any) => console.log(err),
-                () => console.log('get istituto api'));
-    }
+  initPieChart(report) {
 
-    menuContentShow() {
-        this.showContent = !this.showContent;
-    }
+    this.pieChartLabels =[];
+    this.pieChartData = [];
+ 
+    this.samplePieChartData();
+    
+   
+  }
 
-    modifica() {
-        this.router.navigate(['../modifica', this.dataService.istitutoId], { relativeTo: this.route });
-      }
+  samplePieChartData() {
+    this.tipologie.forEach(element => {
+      this.pieChartLabels.push(element.titolo);
+      this.pieChartData.push(Math.floor(Math.random() * 8) + 1  );
+      console.log(element.titolo);
+    });
+  }
+
+  getIstituto() {
+    this.dataService.getIstitutoById(this.dataService.istitutoId)
+      .subscribe((response) => {
+        this.istituto = response;
+      },
+        (err: any) => console.log(err),
+        () => console.log('get istituto api'));
+  }
+
+  menuContentShow() {
+    this.showContent = !this.showContent;
+  }
+
+  modifica() {
+    this.router.navigate(['../modifica', this.dataService.istitutoId], { relativeTo: this.route });
+  }
 
 }
