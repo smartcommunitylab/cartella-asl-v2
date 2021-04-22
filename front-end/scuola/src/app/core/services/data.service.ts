fede7193773493b44de170666b5ad8832b8839e5
Nawaz Khurshid <nawaz1981@gmail.com>
2021-04-22 12:25:38 +0200

    Update data.service.ts
    Fix for asl2-380

--- front-end/scuola/src/app/core/services/data.service.ts
+++ front-end/scuola/src/app/core/services/data.service.ts
@@ -1354,6 +1354,26 @@ export class DataService {
     return forkJoin(singleObservables);
   }
 
+  getDashboardIstitutoClasse(): Observable<String[]> {
+    let url = this.host + '/dashboard-ist/classi';
+    let params = new HttpParams();
+    params = params.append('istitutoId', this.istitutoId);
+    params = params.append('annoScolastico', this.schoolYear);
+    
+    return this.http.get<String[]>(url,
+      {
+        observe: 'response',
+        params: params
+      })
+      .timeout(this.timeout)
+      .pipe(
+        map(res => {
+          return res.body;
+        }),
+        catchError(this.handleError)
+      );
+  }
+
   apiFormat(D) {
     var yyyy = D.getFullYear().toString();
     var mm = (D.getMonth() + 1).toString(); // getMonth() is zero-based         
