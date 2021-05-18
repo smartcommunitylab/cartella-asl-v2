4daf6d936fe27d475ef0bd6fd1e93797215d3eb3
Nawaz Khurshid <nawaz1981@gmail.com>
2021-05-18 10:52:54 +0200

    Fix for asl2-385

--- front-end/scuola/src/app/piani/actions/new-piano-modal/new-piano-modal.component.ts
+++ front-end/scuola/src/app/piani/actions/new-piano-modal/new-piano-modal.component.ts
@@ -10,12 +10,14 @@ export class NewPianoModalComponent implements OnInit {
 
   titolo: string;
   annoRiferimento: any;
+  monte2anno: number;
   monte3anno: number;
   monte4anno: number;
   monte5anno: number;
   note: string = '';
   fieldsError: string;
   corsoStudioId: any = 'Corso di studio';
+  quadriennali: boolean = false;
     
   @Input() corsiStudio?: any; //list of corsi studio
   @Output() newPianoListener = new EventEmitter<Object>();
@@ -29,21 +31,43 @@ export class NewPianoModalComponent implements OnInit {
     
   }
 
+  onChange(corsoId) {
+    console.log(this.corsiStudio.find(corso => corso.courseId == corsoId).nome);
+    let corsoSperimentale = this.corsiStudio.find(corso => corso.courseId == corsoId).corsoSperimentale;
+    if (corsoSperimentale) {
+      this.quadriennali = true;
+    } else {
+      this.quadriennali = false;
+    }
+  }
+
   create() { //create or update
     let piano;
     let year = this.annoRiferimento.substring(0, 4);
     if (this.allValidated()) {
       console.log(this.corsoStudioId);
-      piano = {
-        titolo: this.titolo.trim(),
-        corsoDiStudioId: this.corsoStudioId,
-        corsoDiStudio: this.corsiStudio.find(corso => corso.courseId == this.corsoStudioId).nome,
-        note: this.note,
-        oreTerzoAnno: this.monte3anno,
-        oreQuartoAnno: this.monte4anno,
-        oreQuintoAnno: this.monte5anno,
-        // dataAttivazione: new Date(year + "-09").getTime() //starts from september
+      if (!this.quadriennali) {
+        piano = {
+          titolo: this.titolo.trim(),
+          corsoDiStudioId: this.corsoStudioId,
+          corsoDiStudio: this.corsiStudio.find(corso => corso.courseId == this.corsoStudioId).nome,
+          note: this.note,
+          oreTerzoAnno: this.monte3anno,
+          oreQuartoAnno: this.monte4anno,
+          oreQuintoAnno: this.monte5anno,
+        }
+      } else {
+        piano = {
+          titolo: this.titolo.trim(),
+          corsoDiStudioId: this.corsoStudioId,
+          corsoDiStudio: this.corsiStudio.find(corso => corso.courseId == this.corsoStudioId).nome,
+          note: this.note,
+          oreSecondoAnno: this.monte2anno,
+          oreTerzoAnno: this.monte3anno,
+          oreQuartoAnno: this.monte4anno,
+        }
       }
+      
       this.newPianoListener.emit(piano);
       this.activeModal.dismiss('create')
     } else {
@@ -53,14 +77,27 @@ export class NewPianoModalComponent implements OnInit {
 
 
   allValidated() {
-    return (
-      (this.titolo && this.titolo != '' && this.titolo.trim().length > 0)
-      && (this.monte3anno && this.monte3anno > 0)
-      && (this.monte4anno && this.monte4anno > 0)
-      && (this.monte5anno && this.monte5anno > 0)
-      && (this.annoRiferimento && this.annoRiferimento != '')
-      && (this.corsoStudioId && this.corsoStudioId != 'Corso di studio')
-      );
+    
+    if (!this.quadriennali) {
+      return (
+        (this.titolo && this.titolo != '' && this.titolo.trim().length > 0)
+        && (this.monte3anno && this.monte3anno > 0)
+        && (this.monte4anno && this.monte4anno > 0)
+        && (this.monte5anno && this.monte5anno > 0)
+        && (this.annoRiferimento && this.annoRiferimento != '')
+        && (this.corsoStudioId && this.corsoStudioId != 'Corso di studio')
+        );
+    } else {
+      return (
+        (this.titolo && this.titolo != '' && this.titolo.trim().length > 0)
+        && (this.monte2anno && this.monte2anno > 0)
+        && (this.monte3anno && this.monte3anno > 0)
+        && (this.monte4anno && this.monte4anno > 0)
+        && (this.annoRiferimento && this.annoRiferimento != '')
+        && (this.corsoStudioId && this.corsoStudioId != 'Corso di studio')
+        );
+    }
+    
   }
   
   trimValue(event, type) {
