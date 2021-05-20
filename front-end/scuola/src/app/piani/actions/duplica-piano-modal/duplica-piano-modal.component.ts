d87d1b48ac539df09830b9145ed8c48ee112910d
Nawaz Khurshid <nawaz1981@gmail.com>
2021-05-20 09:48:58 +0200

    Fix for duplica piano

--- front-end/scuola/src/app/piani/actions/duplica-piano-modal/duplica-piano-modal.component.ts
+++ front-end/scuola/src/app/piani/actions/duplica-piano-modal/duplica-piano-modal.component.ts
@@ -39,14 +39,25 @@ export class DuplicaPianoModal implements OnInit {
 
 
   allValidated() {
-    return (
-      (this.piano.titolo || this.piano.titolo != '')
-      && (this.piano.oreTerzoAnno || this.piano.oreTerzoAnno > 0)
-      && (this.piano.oreQuartoAnno || this.piano.oreQuartoAnno > 0)
-      && (this.piano.oreQuintoAnno || this.piano.oreQuintoAnno > 0)
-      && (this.annoRiferimento || this.annoRiferimento != '')
-      && (this.piano.corsoDiStudioId && this.piano.corsoDiStudioId != 'Corso di studio')
-    );
+    if (this.piano.corsoSperimentale) {
+      return (
+        (this.piano.titolo || this.piano.titolo != '')
+        && (this.piano.oreSecondoAnno || this.piano.oreSecondoAnno > 0)
+        && (this.piano.oreTerzoAnno || this.piano.oreTerzoAnno > 0)
+        && (this.piano.oreQuartoAnno || this.piano.oreQuartoAnno > 0)
+        && (this.annoRiferimento || this.annoRiferimento != '')
+        && (this.piano.corsoDiStudioId && this.piano.corsoDiStudioId != 'Corso di studio')
+      );
+    } else {
+      return (
+        (this.piano.titolo || this.piano.titolo != '')
+        && (this.piano.oreTerzoAnno || this.piano.oreTerzoAnno > 0)
+        && (this.piano.oreQuartoAnno || this.piano.oreQuartoAnno > 0)
+        && (this.piano.oreQuintoAnno || this.piano.oreQuintoAnno > 0)
+        && (this.annoRiferimento || this.annoRiferimento != '')
+        && (this.piano.corsoDiStudioId && this.piano.corsoDiStudioId != 'Corso di studio')
+      );
+    }    
   }
 
 }
