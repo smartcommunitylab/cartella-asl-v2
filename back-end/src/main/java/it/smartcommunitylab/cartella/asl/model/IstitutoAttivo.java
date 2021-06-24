718171e1c50593d7bab25929970dd9be5240de48
micnori <micnori@yahoo.com>
2021-06-24 14:41:44 +0200

    add istituto attivo

---
+++ back-end/src/main/java/it/smartcommunitylab/cartella/asl/model/IstitutoAttivo.java
@@ -0,0 +1,44 @@
+package it.smartcommunitylab.cartella.asl.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "istituto_attivo")
+public class IstitutoAttivo {
+  @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+  private String istitutoId;
+  private String nome;
+  private String extId;
+
+  public String getExtId() {
+    return extId;
+  }
+  public void setExtId(String extId) {
+    this.extId = extId;
+  }
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public String getIstitutoId() {
+    return istitutoId;
+  }
+  public void setIstitutoId(String istitutoId) {
+    this.istitutoId = istitutoId;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
