6c1a5476f42c3591f0e87928d6ab6901baf29e85
Nawaz Khurshid <nawaz1981@gmail.com>
2021-08-10 15:37:21 +0200

    Fixes

--- front-end/scuola/src/app/core/interceptors/auth.interceptor.ts
+++ front-end/scuola/src/app/core/interceptors/auth.interceptor.ts
@@ -1,15 +1,29 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { SERVER_API_URL } from '../../app.constants';
+import { AuthService } from '../auth/auth.service';
  
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
- 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const authHeader = `Bearer ${sessionStorage.getItem('access_token')}`; 
-    // let authHeader =  "Bearer 8f09a44d-86be-42b9-9830-686a13b6d361";
-    const authReq = req.clone({headers: req.headers.set('Authorization', authHeader)});
-    return next.handle(authReq);
+  constructor(private authService: AuthService) {}
+  
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    // if (!request || !request.url || (request.url.startsWith('http') && !(SERVER_API_URL && request.url.startsWith(SERVER_API_URL)))) {
+    //   return next.handle(request);
+    // }
+
+    const token = this.authService.getAuthorizationHeaderValue();
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: token
+        }
+      });
+    }
+    
+    return next.handle(request);
   }
+
 }
