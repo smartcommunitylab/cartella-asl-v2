85a34f6a3cc3e34f65d50c4b59bfdd0004cc4883
Nawaz Khurshid <nawaz1981@gmail.com>
2021-08-12 12:08:44 +0200

    Update config.ts

--- front-end/azienda/src/app/config.ts
+++ front-end/azienda/src/app/config.ts
@@ -2,6 +2,7 @@ export var config = {
   defaultPosition: { 'latitude': 46.1025748, 'longitude': 10.927261 },
   aacClientId: "",
   redirectUrl: "http://localhost:4200/",
-  scope: "profile.basicprofile.me profile.accountprofile.me",
+  logout_redirect: 'asl-login/',
+  scope: "profile openid email",
   aacUrl: ""
 }
