02994233261b94de2f747971f90b7046719e063a
micnori <micnori@yahoo.com>
2021-08-23 16:27:27 +0200

    Update ConvenzioneController.java

--- back-end/src/main/java/it/smartcommunitylab/cartella/asl/controller/ConvenzioneController.java
+++ back-end/src/main/java/it/smartcommunitylab/cartella/asl/controller/ConvenzioneController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,7 @@ public class ConvenzioneController implements AslController {
 		return result;
 	}
 	
+	@DeleteMapping("api/convenzione/{convenzioneId}")
 	public @ResponseBody Convenzione deleteConvenzione(
 			@PathVariable Long convenzioneId,
 			@RequestParam String istitutoId,
