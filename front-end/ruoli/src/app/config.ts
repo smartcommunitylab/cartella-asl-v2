d9978988fbc4341b2704a0e2ef95b32ab13e7401
ffais <ffais@fbk.eu>
2021-09-15 12:01:34 +0200

    update config.ts

--- front-end/ruoli/src/app/config.ts
+++ front-end/ruoli/src/app/config.ts
@@ -1,8 +1,8 @@
 export var config = {
   defaultPosition: { 'latitude': 46.1025748, 'longitude': 10.927261 },
-  aacClientId: "",
-  redirectUrl: "",
+  aacClientId: "c_37b06a4d-9ca5-47bb-8630-283249916cdd",
+  redirectUrl: "https://cartella-asl-demo.platform.smartcommunitylab.it/asl-ruoli-v2/",
   logout_redirect: 'asl-login/',
   scope: "profile openid email",
-  aacUrl: ""
+  aacUrl: "https://aac-adc.platform.smartcommunitylab.it"
 }
--- front-end/scuola/src/app/config.ts
+++ front-end/scuola/src/app/config.ts
@@ -1,7 +1,7 @@
 export var config = {
   defaultPosition: { 'latitude': 46.1025748, 'longitude': 10.927261 },
   aacClientId: "c_37b06a4d-9ca5-47bb-8630-283249916cdd",
-  redirectUrl: "http://localhost:4300/",
+  redirectUrl: "https://cartella-asl-demo.platform.smartcommunitylab.it/asl-scuola-v2/",
   logout_redirect: 'asl-login/',
   scope: "profile openid email",
   aacUrl: "https://aac-adc.platform.smartcommunitylab.it"
