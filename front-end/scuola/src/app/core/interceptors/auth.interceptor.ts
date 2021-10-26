002dd0580db267b846bb92b9482508e6f83977bb
Nawaz Khurshid <nawaz1981@gmail.com>
2021-10-26 14:32:16 +0200

    Fixes

--- front-end/scuola/src/app/core/interceptors/auth.interceptor.ts
+++ front-end/scuola/src/app/core/interceptors/auth.interceptor.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../auth/auth.service';
+import { environment } from '../../../environments/environment';
  
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
   
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req.url.indexOf('/cartella-asl/api') > 0) {
+    if (req.url.includes(environment.serverAPIURL)) {
         const token = this.authService.getAuthorizationHeaderValue();
       if (token) {
         req = req.clone({
