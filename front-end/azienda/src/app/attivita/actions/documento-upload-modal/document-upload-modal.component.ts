5ac89f258d2aeb146b6b29834dc2197f558858ab
Nawaz Khurshid <nawaz1981@gmail.com>
2021-11-25 09:23:33 +0100

    Fixes for typos

--- front-end/azienda/src/app/attivita/actions/documento-upload-modal/document-upload-modal.component.ts
+++ front-end/azienda/src/app/attivita/actions/documento-upload-modal/document-upload-modal.component.ts
@@ -12,8 +12,8 @@ export class DocumentUploadModalComponent implements OnInit {
   selectedFileName;
   saveFileObj = { type: null, file: null };
   optionTypes = {
-    "1": "doc_generico",
-    "2": "valutazione_studente"
+    "1": "valutazione_studente",
+    "2": "doc_generico",
   }
   @Input() attivitaIndividuale: boolean;
   @Output() newDocumentListener = new EventEmitter<Object>();
