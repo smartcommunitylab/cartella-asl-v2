df25767b3dc31d96a797396745b1aef73d492c51
Nawaz Khurshid <nawaz1981@gmail.com>
2021-11-30 10:34:05 +0100

    Update attivita-dettaglio.component.ts

--- front-end/scuola/src/app/attivita/actions/attivita-dettaglio/attivita-dettaglio.component.ts
+++ front-end/scuola/src/app/attivita/actions/attivita-dettaglio/attivita-dettaglio.component.ts
@@ -266,6 +266,7 @@ export class AttivitaDettaglioComponent implements OnInit {
   openDocumentUpload() {
     const modalRef = this.modalService.open(DocumentUploadModalComponent, { windowClass: "documentUploadModalClass" });
     modalRef.componentInstance.attivitaIndividuale = this.individuale;
+    modalRef.componentInstance.tipologiaId = this.attivita.tipologia;
     modalRef.componentInstance.newDocumentListener.subscribe((option) => {
       console.log(option);
       this.dataService.uploadDocumentToRisorsa(option, this.attivita.uuid + '').subscribe((doc) => {
